46d4e5d08a5a2798f6c773c3c2d711766f3561f6
Priyansh <priyashpatel21@gnu.ac.in>
2023-07-31 08:12:25 +0530

    P2

--- app/src/main/java/com/priyanshpatel/mad_practical2_21012011100/MainActivity.kt
+++ app/src/main/java/com/priyanshpatel/mad_practical2_21012011100/MainActivity.kt
@@ -1,28 +1,59 @@
-package com.priyanshpatel.mad_practical2_21012011100
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.SnackbarLayout
+
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity()"
-     fun showMessage(message:String){
-         Log.i(TAG, message: )
-         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-     }
-
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-        showMessage("onCreate method is called")
-
+        showMsg("onCreate method is called.")
     }
-    override fun onStart(){
+
+    override fun onStart() {
         super.onStart()
-        showMessage("onStart method is called")
+        showMsg("onStart method is called.")
     }
 
-    override fun onCreate
+    override fun onResume() {
+        super.onResume()
+        showMsg("onResume method is called.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMsg("onPause method is called.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMsg("onStop method is called.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMsg("onDestroy method is called.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMsg("onRestart method is called.")
+    }
+
+
+    fun showMsg(message: String) {
+        Log.i(TAG, message)
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+        val Constraint: ConstraintLayout? = findViewById(R.id.mainConstraint)
+        if (Constraint != null) {
+            Snackbar.make(Constraint, message, Snackbar.LENGTH_SHORT).show()
+        }
+    }
 }
